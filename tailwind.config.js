adceee78c8f4e77ce3edb7e0f1091953355674b6
Abhay <rabhay780@gmail.com>
2024-09-21 15:11:07 +0530

    V1.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/*.{html,js}"],
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inria_Sans: ["Inria Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
